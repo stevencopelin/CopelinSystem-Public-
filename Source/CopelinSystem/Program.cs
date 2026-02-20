c85bbb114eebf2f3705843a2e445b1ac4fbdc70b
Steve Copelin <steven.copelin@hpw.qld.gov.au>
2026-02-20 13:21:34 +1000

    Disable HTTPS redirection

--- Source/CopelinSystem/Program.cs
+++ Source/CopelinSystem/Program.cs
@@ -176,7 +176,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 // Add authentication and authorization middleware - ORDER IS IMPORTANT
 app.UseAuthentication();
